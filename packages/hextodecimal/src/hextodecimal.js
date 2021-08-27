ee5e3303ffafddae3d14203a5816486deb083cd9
Corbin Crutchley <crutchcorn@gmail.com>
2021-08-27 15:06:13 -0700

    Added initial hextodecimal logic and tests

--- packages/hextodecimal/src/hextodecimal.js
+++ packages/hextodecimal/src/hextodecimal.js
@@ -1,1 +1,12 @@
-module.exports = function () {}
+const isValidHex = /^-?[0-9A-Fa-f ]+$/
+
+module.exports = function (hexStr) {
+    if (!isValidHex.exec(hexStr)) {
+        throw new Error("Unexpected value. Please input valid HEX")
+    }
+    const number = parseInt(hexStr.replace(' ', ''), 16);
+    if (Number.isNaN(number)) {
+        throw new Error("Unexpected value. Please input valid HEX")
+    }
+    return number;
+}
