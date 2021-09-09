c92a550c95aaf6eba10b29968c3da63bd6776e73
bobrossrtx <bobrossrtx@gmail.com>
2021-09-10 00:01:09 +0100

    fix: Added new line characters to remove XTERM invalid endlines

--- packages/binarytohex/src/binarytohex.js
+++ packages/binarytohex/src/binarytohex.js
@@ -2,11 +2,11 @@ const isValidBinary = /^-?[01\s]+$/
 
 export default function (binaryStr) {
     if (!isValidBinary.exec(binaryStr)) {
-        throw new Error("Unexpected value. Please input valid decimal")
+        throw new Error("Unexpected value. Please input valid decimal\n")
     }
     const number = parseInt(binaryStr.replace(/\s/g, ''), 2);
     if (Number.isNaN(number)) {
-        throw new Error("Unexpected value. Please input valid HEX")
+        throw new Error("Unexpected value. Please input valid HEX\n")
     }
     return number.toString(16);
 }
--- packages/hextodecimal/src/hextodecimal.js
+++ packages/hextodecimal/src/hextodecimal.js
@@ -2,11 +2,11 @@ const isValidHex = /^-?[0-9A-Fa-f\s]+$/
 
 export default function (hexStr) {
     if (!isValidHex.exec(hexStr)) {
-        throw new Error("Unexpected value. Please input valid HEX")
+        throw new Error("Unexpected value. Please input valid HEX\n")
     }
     const number = parseInt(hexStr.replace(/\s/g, ''), 16);
     if (Number.isNaN(number)) {
-        throw new Error("Unexpected value. Please input valid HEX")
+        throw new Error("Unexpected value. Please input valid HEX\n")
     }
     return number.toString(10);
 }
